ec52dc90c78f05556ad82b180a10bf2f04edeb99
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-22 20:12:02 +0900

    main.jsx修正

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    ReactDOM.createRoot(document.getElementById("root")!).render(
           <BrowserRouter basename={import.meta.env.DEV ? "/" : "HarukiSakai_Portfolio"}>
     <App />
     </BrowserRouter>
-    );
   </StrictMode>,
 )
