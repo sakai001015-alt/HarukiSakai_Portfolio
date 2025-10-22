7a21d1cd10ebfcd1107263304370d48740c4dc94
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-22 20:41:00 +0900

    main.jsx元に戻した

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-          <BrowserRouter basename={import.meta.env.DEV ? "/" : "/HarukiSakai_Portfolio/"}>
     <App />
-    </BrowserRouter>
   </StrictMode>,
 )
