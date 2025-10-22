2e460cfe0272a427d1fbb1ac52377a80219bb255
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-22 19:54:12 +0900

    App.jsx再修正

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="app-container">
         <ParticlesBackground />
         ReactDOM.createRoot(document.getElementById("root")!).render(
-          <BrowserRouter basename={import.meta.env.DEV ? "/" : "/レポジトリ名/"}>
+          <BrowserRouter basename={import.meta.env.DEV ? "/" : "HarukiSakai_Portfolio"}>
           <div className="page">
             <Header />
             <Routes>
