9d14f8153901bcfbc30f677f8771ff77f4513ce4
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-23 04:48:28 +0900

    main.jsx,App.jsx修正

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import HomePage from "./pages/HomePage";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-    <Route path={ROUTES.HOME} element={<HomePage />} />
-    </Routes>
-    </BrowserRouter>
     <App />
-    
   </StrictMode>
 )
