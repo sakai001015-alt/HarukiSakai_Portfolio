e71afadd7816c11ffc40c011523aa1b83d61973b
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-23 10:02:49 +0900

    修正

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <>
         <div className={styles.container} >
         <h1>Haruki Sakai のポートフォリオサイト</h1>
-        <img src={`${base}31411.png`} alt="icon" />
+        <img src="31411.png" alt="icon" />
         </div>
     </>
   )
