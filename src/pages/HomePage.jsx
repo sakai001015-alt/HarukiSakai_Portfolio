9ab8226df70161d272d74cb7eafaa0ce397bb184
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-23 09:44:18 +0900

    修正

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
+  const base = process.env.GITHUB_PAGES ? '/HarukiSakai_Portfolio/' : './';
   return (
     <>
         <div className={styles.container} >
