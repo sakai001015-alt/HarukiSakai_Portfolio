58fe546a48501bc90d9b5ee7329b3f4dde0d3f32
HarukiSakai_Main <sakai001015@gmail.com>
2025-10-23 11:48:01 +0900

    修正

--- src/components/card/SkillCard.jsx
+++ src/components/card/SkillCard.jsx
@@ -9,7 +9,7 @@ export default function SkillCard({ name, level, detail }) {
       <div className={styles.container}>
         <div className={styles.content}>
           <h2 className={styles.title}>{name}</h2>
-          <p className={styles.img}><img src={`${APP_NAME}/star_${level}.png`}></img></p>
+          <p className={styles.img}><img src={`./star_${level}.png`}></img></p>
           <p className={styles.description}>{detail}</p>
         </div>
       </div>
